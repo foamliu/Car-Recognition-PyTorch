7d7c5565cbdc60072833feb7298f9dd54da861d2
Yang Liu <liuyang12@focusmedia.cn>
2019-12-13 09:37:13 +0800

    update

--- test.py
+++ test.py
@@ -1,26 +1,39 @@
 import os
 import time
 
-import cv2 as cv
-import keras.backend as K
 import numpy as np
+import torch
+from PIL import Image
 from tqdm import tqdm
 
-from utils import load_model
+from config import device
+from data_gen import data_transforms
+from models import CarRecognitionModel
 
 if __name__ == '__main__':
-    model = load_model()
+    filename = 'car_recognition.pt'
+    model = CarRecognitionModel()
+    model.load_state_dict(torch.load(filename))
+    model = model.to(device)
+    model.eval()
+
+    transformer = data_transforms['valid']
 
     print('Predicting test data')
     num_samples = 8041
     start = time.time()
     out = open('result.txt', 'a')
     for i in tqdm(range(num_samples)):
-        filename = os.path.join('data/test', '%05d.jpg' % (i + 1))
-        bgr_img = cv.imread(filename)
-        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
-        rgb_img = np.expand_dims(rgb_img, 0)
-        preds = model.predict(rgb_img)
+        full_path = os.path.join('data/test', '%05d.jpg' % (i + 1))
+        img = Image.open(full_path)
+        img.save('images/{}_out.png'.format(i))
+        img = transformer(img)
+        imgs = img.unsqueeze(dim=0)
+
+        with torch.no_grad():
+            preds = model(imgs)
+
+        preds = preds.cpu().numpy()[0]
         prob = np.max(preds)
         class_id = np.argmax(preds)
         out.write('{}\n'.format(str(class_id + 1)))
@@ -30,4 +43,3 @@ if __name__ == '__main__':
     print('avg fps: {}'.format(str(num_samples / seconds)))
 
     out.close()
-    K.clear_session()
