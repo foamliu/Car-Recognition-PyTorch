96b4d55650cdd136cb74a1056533bda533590237
Yang Liu <liuyang12@focusmedia.cn>
2019-12-23 16:57:20 +0800

    update

--- models.py
+++ models.py
@@ -8,18 +8,18 @@ from config import num_classes
 class CarRecognitionModel(nn.Module):
     def __init__(self):
         super(CarRecognitionModel, self).__init__()
-        resnet = models.mobilenet_v2(pretrained=True)
+        resnet = models.resnet50(pretrained=True)
         # Remove linear layer
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
-        self.pool = nn.AvgPool2d(kernel_size=7)
-        self.fc = nn.Linear(1280, num_classes)
+        # self.pool = nn.AvgPool2d(kernel_size=7)
+        self.fc = nn.Linear(2048, num_classes)
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, images):
         x = self.resnet(images)  # [N, 2048, 7, 7]
-        x = self.pool(x)
-        x = x.view(-1, 1280)  # [N, 2048]
+        # x = self.pool(x)
+        x = x.view(-1, 2048)  # [N, 2048]
         x = self.fc(x)
         x = self.softmax(x)
         return x
--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import numpy as np
 import torch
 from torch import nn
+from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.tensorboard import SummaryWriter
 
 from config import device, grad_clip, print_freq, num_workers
 from data_gen import CarRecognitionDataset
 from models import CarRecognitionModel
-from utils import parse_args, save_checkpoint, AverageMeter, clip_gradient, get_logger, accuracy, get_learning_rate, \
-    adjust_learning_rate
+from utils import parse_args, save_checkpoint, AverageMeter, clip_gradient, get_logger, accuracy, get_learning_rate
 
 
 def train_net(args):
@@ -24,8 +24,9 @@ def train_net(args):
         model = CarRecognitionModel()
         model = nn.DataParallel(model)
 
-        # optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, nesterov=True, weight_decay=1e-6)
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum,
+                                    weight_decay=args.weight_decay)
+        # optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
     else:
         checkpoint = torch.load(checkpoint)
@@ -50,16 +51,10 @@ def train_net(args):
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False,
                                                num_workers=num_workers)
 
-    # scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.1)
+    scheduler = MultiStepLR(optimizer, milestones=[30, 80], gamma=0.1)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
-        if epochs_since_improvement > 50:
-            break
-        # Decay learning rate if there is no improvement for 10 consecutive epochs
-        if epochs_since_improvement > 0 and epochs_since_improvement % 12 == 0:
-            adjust_learning_rate(optimizer, 0.1)
-
         # One epoch's training
         train_loss, train_acc = train(train_loader=train_loader,
                                       model=model,
@@ -82,6 +77,8 @@ def train_net(args):
 
         writer.add_scalar('model/valid_loss', valid_loss, epoch)
         writer.add_scalar('model/valid_accuracy', valid_acc, epoch)
+
+        scheduler.step(epoch)
 
         # Check if there was an improvement
         is_best = valid_acc > best_acc
